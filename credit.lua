1dd142f1a933da92f047e1a2bf4ea9696bca7c07
kexline <kexline@@users.noreply.github.com>
2015-04-12 16:38:25 -0500

    credit.lua completed
    Quirks:  Scrollbar not showing up as expected, and may be misaligned in
    the image sheet.  Scrollview (the text box) disappears if inserted into
    sceneGroup, so I told it to remove itself in the hide function.

--- credit.lua
+++ credit.lua
@@ -6,6 +6,7 @@
 
 -- References:  
 -- buttons created using http://dabuttonfactory.com/ and sheeted in Inkscape 
+-- Block text solution:  https://coronalabs.com/blog/2014/06/17/tutorial-working-with-large-blocks-of-text/
 
 -- includes and physics
 local composer = require("composer")
@@ -27,21 +28,45 @@ local yy = display.contentCenterY; local hh=display.contentHeight;
 local fs=hh/20;  local ff=native.systemFont;
 
 -- scene variables
-local function shuffleTable( t )
-    assert( t, "shuffleTable() expected a table, got nil" )
-    local iterations = #t
-    local j
-    
-    for i = iterations, 2, -1 do
-        j = math.random(1,i)
+
+local scrollView; -- have to make this a scene global because it is not working in scenegroup
+
+local function shakeShortTable( t )
+	-- shuffleTable function from previous homework does not work for this very short case.
+    assert( t, "jumbleTable() expected a table, got nil" )
+    local items=#t 
+    local j 
+    j = math.random(1,#t)
+    t[1], t[j] = t[j], t[1];
+
+    for i = items, 1, 1 do
+        j = math.random(1,#t)
         t[i], t[j] = t[j], t[i]
     end
+    return t;
 end
 
-local authors={"Karen Exline", "James Taylor", "Cozette Napoles"};
-shuffleTable(authors);
+-- randomize order of credits!
+local authors={"Cozette Napoles", "James Taylor", "Karen Exline"};
+authors=shakeShortTable(authors);
 
-local creditsBlurb=string.format("Created for UAH by %s, %s, and %s. \n This should appear on a new line.", authors[1], authors[2], authors[3]);
+local blurb=string.format("%s, %s, \n and %s", authors[1], authors[2], authors[3]);	
+
+blurb=[[Created for UAH by 
+]]..blurb.."\n\n"..[[ with help from
+
+Sun-il Kim
+Corona Labs
+dabuttonfactory.com
+
+Rooms inspired by 
+Flash Point Fire Rescue
+
+Other art provided by 
+spritersresource.com
+
+]]
+
 -- Graphics
 local titleOpts = 
 { frames = {
@@ -60,9 +85,20 @@ local btnOptions = { frames = {
 	{ x = 322, y =532, width = 179, height = 65},  -- tutorial huge light
 	{ x = 0, y =756, width = 179, height = 65},  -- credits huge
 	{ x = 322, y =756, width = 179, height = 65},  -- credits huge light
+	{ x = 0, y =85, width = 183, height = 68},  -- continue huge
+	{ x = 322, y =85, width = 183, height = 68},  -- continue huge light
+
 }}
 
 local btnSheet = graphics.newImageSheet( "./images/btnSheet_l.png", btnOptions );
+
+local sbOptions = { frames = {
+	{ x = 7, y =3, width = 17, height = 17}, -- top
+	{ x = 7, y =23, width = 17, height = 6}, -- mid
+	{ x = 7, y =49, width = 17, height = 12}, -- bot
+}}
+
+local sbSheet = graphics.newImageSheet( "./images/scrollBarSheet.png", sbOptions );
 
 
 function scene:create(event)
@@ -101,40 +137,93 @@ function scene:show (event)
 		bxwidth=math.max(ww/400, 400/ww)*.75; 
 		bxheight=math.max(hh/450, 450/hh)*.5;
 		local bx1=display.newImage(titleSheet,1);
-		bx1.x=xx; bx1.y=yy*1.2;
+		bx1.x=xx; bx1.y=yy*1.1;
 		bx1:scale(bxwidth, bxheight); 
 
 		-- display.newRoundedRect( parent, x, y, width, height, cornerRadius )
-		local bx2=display.newRoundedRect(xx, yy*1.2, bxwidth*.82*400, bxheight*.82*450, 35);
+		local bx2=display.newRoundedRect(xx, yy*1.1, bxwidth*.82*400, bxheight*.82*450, 35);
 		bx2:setFillColor(1,1,1)
 		bx2:setStrokeColor(0.39, 0.23, 0, 0.4); bx2.strokeWidth=10;
 		bx2:toFront();	
 
 		sceneGroup:insert(bx1);
+		sceneGroup:insert(bx2);
 
-		-- Main Menu button =====================================
 
-		btn1=widget.newButton({
-			x=xx, y=yy+20,
-			id="mainmenu", onPress=btnMain, 
-			sheet = btnSheet,
-			defaultFrame = 1,
-			overFrame = 2,
-			strokeWidth=2,
-		}) 
+		local paragraphs = {}
+		local paragraph
+		local tmpString = blurb
 
-		sceneGroup:insert(btn1);
+		local scrollw=bxwidth*.82*400*.9;
+		local scrollh=bxheight*.82*400*.9;
+
+		scrollView = widget.newScrollView
+		{
+		    top = yy*1.1-scrollh*.5,
+		    left = xx-scrollw*.5-10,
+		    width = scrollw,
+		    height = scrollh,
+		    scrollWidth = scrollw-10,
+		    scrollHeight = 8000,
+
+		    hideScrollBar = false,
+			scrollBarOptions = {
+			    sheet = sbSheet, 		 --reference to the image sheet
+			    topFrame = 1,            --number of the "top" frame
+			    middleFrame = 2,         --number of the "middle" frame
+			    bottomFrame = 3          --number of the "bottom" frame
+			}
+
+		}
+
+		-- Why does inserting scrollview into scenegroup make it disappear?
+		-- sceneGroup:insert(scrollView);
+		-- scrollView:toFront();
+
+		local options = {
+		    text = "",
+		    width = scrollh,
+		    font = ff,
+		    fontSize = fs*.45,
+		    align = "center"
+
+		}
+
+		local yOffset = 10
+
+		repeat
+		    paragraph, tmpString = string.match( tmpString, "([^\n]*)\n(.*)" )
+		    options.text = paragraph
+		    paragraphs[#paragraphs+1] = display.newText( options )
+		    paragraphs[#paragraphs].anchorX = 0
+		    paragraphs[#paragraphs].anchorY = 0
+		    paragraphs[#paragraphs].x = 10
+		    paragraphs[#paragraphs].y = yOffset
+		    paragraphs[#paragraphs]:setFillColor( 0.39, 0.23, 0  )
+		    scrollView:insert( paragraphs[#paragraphs] )
+		    yOffset = yOffset + paragraphs[#paragraphs].height
+		    print( #paragraphs, paragraph )
+		until tmpString == nil or string.len( tmpString ) == 0
+
 
 	elseif phase=="did" then
 
-		creditText=display.newText(creditsBlurb, xx, yy, ff, fs*.4);
-		creditText:setFillColor( 0.39, 0.23, 0 )
+		-- Main Menu button =====================================
 
-		-- local scrollView = widget.newScrollView{
-    --     top = myTop, left = myLeft,
-    --     width = someWidth, height = someHeight,
-    --     bgColor = { 255, 255, 255, 210 },   --set the RGBA background color.
-    -- }
+		local function btnMain(event)
+			composer.gotoScene("welcome");
+		end
+
+		btn1=widget.newButton({
+			x=xx, y=hh*.9,
+			id="mainmenu", onPress=btnMain, 
+			sheet = btnSheet,
+			defaultFrame = 7,	
+			overFrame = 8,
+			strokeWidth=2,
+		}) 
+		sceneGroup:insert(btn1);
+
 
 	end
 end
@@ -144,6 +233,7 @@ function scene:hide(event)
 	local sceneGroup = self.view;
 
 	if phase=="will" then
+		scrollView:removeSelf();
 
 	elseif phase=="did" then
 
