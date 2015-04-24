a3d391b5a51dbcdec6252dc662cf85b174f101dd
kexline <kexline@@users.noreply.github.com>
2015-04-24 18:17:48 -0500

    Cozette 4/24/2015 17:26

--- Bin.lua
+++ Bin.lua
@@ -3,9 +3,9 @@
 -- Group members: Karen Exline, Cozette Napoles, and James Taylor
 -- Approach:
 
-local CollisionFilters = require ("CollisionFilters");
+-----------------Bin Object - Parent object of Basket and Box----------------------------
 
-local Bin = {score = 0, itemCntr = 10, binSize=150, xPos = 0, yPos = 0}
+local CollisionFilters = require ("CollisionFilters");
 
 ----------------- Convenience variables -------------------------------------------------
 
@@ -16,19 +16,22 @@ local yy = display.contentCenterY; local hh=display.contentHeight;
 
 binOpts = {
 	frames = {
-	{x=75,  y=5, width=145, height=300}, -- bin 
-	{x=255, y=5, width=235, height=300}, -- basket
-	{x=450, y=5, width=100, height=200} -- basket
+	{x=75,  y=5, width=145, height=300}, -- Bin 
+	{x=255, y=5, width=235, height=300}, -- Basket
+	{x=526, y=5, width=262, height=300} -- Box
 	}}
 
 binSheet = graphics.newImageSheet("./images/binSheet.png", binOpts); 
+-------------------------------------------------------------------------------------------
 
------------------Bin Object - Parent object of Basket and Box----------------------------
+local Bin = {score = 0, itemCntr = 10, binSize=150, xPos = 0, yPos = 0}
+-- score: Used to update the player's accuracy
+-- itemCntr: Tracks the number of items for this bin
+-- binSize: Used for scaling the associated art
+-- xPos: X coordinate used for positioning the bin
+-- yPos: Y coordinate used for positioning the bin
 
--- score: Variable used to update the player's accuracy
--- itemCntr: Variable that tracks the number of items/number of items for this bin
-
-function Bin:new (o)    --Constructor
+function Bin:new (o)
   o = o or {}; 
   setmetatable(o, self);
   self.__index = self;
@@ -36,35 +39,35 @@ function Bin:new (o)    --Constructor
 end
 
 function Bin:spawn()
+	self.shape = display.newImage(binSheet, 1, self.xPos, self.yPos)
 
-	-- self.shape = display.newCircle(display.contentCenterX, 200, 65)
-	self.shape = display.newImage(binSheet, 1)
-	self.shape.x=ww*.3; self.shape.y=200;
+	-- Scale the size of the graphic associated with this bin
 	local a=math.min(self.binSize/250, 250/self.binSize);
 	self.shape:scale(a,a);
 
 	self.shape.pp = self
 	self.shape.tag = "trash bin"
-	self.shape:setFillColor(1,1,1)
-	--physics.addBody(self.shape, "static", {filter=CollisionFilters.bin})
+
 	physics.addBody(self.shape, "static", {filter=CollisionFilters.bin, 
-        shape = {-43,-50, 42,-50, 42,62, -43,62 }})
+					shape = {-43,-50, 42,-50, 42,62, -43,62 }})
+
+
 	function removeItem(event)
-		--print(string.format("Remove Item:  Bin caught %s", event.other.tag))
+		-- Remove any trash, laundry, or toy that collides with this bin
 		if (event.phase == "began") then
 			if (event.other.tag == "trash") then --Correct bin
-				if (event.other.score ==1) then
+				if (event.other.score == 1) then -- Item is not over the midline
 					self.score = self.score + 1 --Score increases
 					self.itemCntr = self.itemCntr - 1 --One less item
 					event.other:removeSelf() --Remove the item
 					event.other = nil
 
-				else
+				else -- Item was over the midline, score does NOT increase
 					self.itemCntr = self.itemCntr - 1 --One less item
 					event.other:removeSelf() --Remove the item
 					event.other = nil
 				end
-			else --Wrong bin
+			else -- Wrong bin
 				self.itemCntr = self.itemCntr - 1 --One less item
 				event.other:removeSelf() --Remove the item
 				event.other = nil
