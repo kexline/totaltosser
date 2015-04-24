1403082b773d0ad3adc774826629599165d9b361
kexline <kexline@@users.noreply.github.com>
2015-04-24 18:19:09 -0500

    Cozette 4/24/2015 5:27pm

--- Basket.lua
+++ Basket.lua
@@ -12,42 +12,43 @@ local CollisionFilters = require ("CollisionFilters");
 
 local xx = display.contentCenterX; local ww=display.contentWidth;
 local yy = display.contentCenterY; local hh=display.contentHeight;
+-----------------------------------------------------------------------------------------
 
------------ Basekt Object --------------------------------------------------------------
-
-local Basket = Bin:new({itemCntr = 10, binSize=120, xPos=0, yPos=0}
- -- Number of items generated for this bin.
+local Basket = Bin:new({itemCntr = 10, binSize=120, xPos=0, yPos=0})
+-- itemCntr: Tracks the number of items for this basket
+-- binSize: Used for scaling the associated art
+-- xPos: X coordinate used for positioning the basket
+-- yPos: Y coordinate used for positioning the basket
 
 function Basket:spawn()
-	--self.shape = display.newRect(display.contentCenterX-210, 165, 165, 110)
-
 	self.shape = display.newImage(binSheet, 2, self.xPos, self.yPos)
-	self.shape.x=ww*.8; self.shape.y=210;
+
+	-- Scale the size of the graphic associated with this basket
 	local a=math.min(self.binSize/250, 250/self.binSize);
 	self.shape:scale(a,a-.1);
 
 	self.shape.pp = self
 	self.shape.tag = "basket"
---	physics.addBody(self.shape, "static", {filter=CollisionFilters.basket})
+
 	physics.addBody(self.shape, "static", {filter=CollisionFilters.basket,
-        shape = {-50,-50, 48,-50, 48,56, -50,56 }})
+					shape = {-50,-50, 48,-50, 48,56, -50,56 }})
 
 		function removeItem(event)
-			print(string.format("Remove Item:  Basket caught %s", event.other.tag))
+			-- Remove any trash, laundry, or toy that collides with this basket
 			if (event.phase == "began") then
-
 				if (event.other.tag == "laundry") then --Correct bin
-					if (event.other.score ==1) then
+					if (event.other.score == 1) then  -- Item is not over the midline
 						self.score = self.score + 1 --Score increases
 						self.itemCntr = self.itemCntr - 1 --One less item
 						event.other:removeSelf() --Remove the item
 						event.other = nil
-					else
+
+					else -- Item was over the midline, score does NOT increase
 						self.itemCntr = self.itemCntr - 1 --One less item
 						event.other:removeSelf() --Remove the item
 						event.other = nil
 				end
-				else --Wrong bin
+				else -- Wrong bin
 					self.itemCntr = self.itemCntr - 1 --One less item
 					event.other:removeSelf() --Remove the item
 					event.other = nil
@@ -58,7 +59,4 @@ function Basket:spawn()
 		self.shape:addEventListener("collision", removeItem)
 end
 
-
-
-
-return Basket
+return Basket;
